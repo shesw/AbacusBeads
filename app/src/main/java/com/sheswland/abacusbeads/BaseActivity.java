9fe5cf705e2f2d5fa02177ed9e4c00bfde946a28
shesw <anjicsl14@gmail.com>
2019-08-19 13:00:41 +0800

    add first page

--- app/src/main/java/com/sheswland/abacusbeads/BaseActivity.java
+++ app/src/main/java/com/sheswland/abacusbeads/BaseActivity.java
@@ -3,6 +3,8 @@ package com.sheswland.abacusbeads;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 import org.litepal.LitePal;
 
 public class BaseActivity extends AppCompatActivity {
@@ -11,5 +13,6 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        Fresco.initialize(this);
     }
 }
