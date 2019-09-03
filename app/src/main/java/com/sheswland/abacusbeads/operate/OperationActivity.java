5c957f8476b9c17e1e844a5792e25231a945fe63
shesw <anjicsl14@gmail.com>
2019-09-03 21:19:10 +0800

    rename operate activity layout

--- app/src/main/java/com/sheswland/abacusbeads/operate/OperationActivity.java
+++ app/src/main/java/com/sheswland/abacusbeads/operate/OperationActivity.java
@@ -2,14 +2,8 @@ package com.sheswland.abacusbeads.operate;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -43,7 +37,7 @@ public class OperationActivity extends BaseActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_operate);
 
         mActivity = this;
 
