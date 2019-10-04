a3dde229d75539f70a29323786abe6a48b7eb531
shesw <anjicsl14@gmail.com>
2019-10-04 23:48:17 +0800

    add preview scale

--- app/src/main/java/com/sheswland/abacusbeads/flsts/sinapic/PreviewViewPagerAdapter.java
+++ app/src/main/java/com/sheswland/abacusbeads/flsts/sinapic/PreviewViewPagerAdapter.java
@@ -3,7 +3,6 @@ package com.sheswland.abacusbeads.flsts.sinapic;
 import android.app.Activity;
 import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
@@ -11,14 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.facebook.drawee.view.SimpleDraweeView;
 import com.sheswland.abacusbeads.R;
 import com.sheswland.abacusbeads.utils.DebugLog;
 import com.sheswland.abacusbeads.utils.SinaUtils;
 
-import java.util.List;
 
-public class PreviewViewPagerAdapter extends PagerAdapter implements View.OnClickListener {
+public class PreviewViewPagerAdapter extends PagerAdapter implements PreViewDraweeView.OnEventListener {
 
     private Activity mActivity;
     private ViewPager mViewPager;
@@ -35,12 +32,13 @@ public class PreviewViewPagerAdapter extends PagerAdapter implements View.OnClic
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
 //        return super.instantiateItem(container, position);
         View view = LayoutInflater.from(container.getContext()).inflate(R.layout.sina_pic_preview, container, false);
-        SimpleDraweeView simpleDraweeView = view.findViewById(R.id.preview_pic);
+        PreViewDraweeView simpleDraweeView = view.findViewById(R.id.preview_pic);
         String url = SinaUtils.getInstance().generateUrlByDefault(SinaPicDataController.getInstance().getPath(position));
         DebugLog.d(SinaPictureActivity.TAG, "instantateItem url " + url);
         simpleDraweeView.setImageURI(Uri.parse(url));
         simpleDraweeView.setTag(position);
-        simpleDraweeView.setOnClickListener(this);
+        simpleDraweeView.setOnEventListener(this);
+        simpleDraweeView.reset();
         container.addView(view);
         return view;
     }
@@ -60,11 +58,17 @@ public class PreviewViewPagerAdapter extends PagerAdapter implements View.OnClic
         return view == o;
     }
 
+
     @Override
     public void onClick(View v) {
         int index = (int) v.getTag();
         DebugLog.d(SinaPictureActivity.TAG, index + "");
         mViewPager.setVisibility(View.GONE);
         mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onDoubleClick() {
+
     }
 }
